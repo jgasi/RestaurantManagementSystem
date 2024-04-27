40a871e05e884a209c0ff32d2ca209860db3171d
juraj <juraj@DESKTOP-QAJPGCA>
2024-04-27 14:39:43 +0200

    Kreiran GuiManager za upravljanje sa User Controls.

---
+++ RestaurantManagementSystem/GuiManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RestaurantManagementSystem
+{
+    public static class GuiManager
+    {
+        public static MainWindow mainWindow { get; set; }
+
+        private static UserControl currentContent;
+        private static UserControl previousContent;
+
+        public static void OpenContent(UserControl userControl)
+        {
+            previousContent = mainWindow.contentPanel.Content as UserControl;
+            mainWindow.contentPanel.Content = userControl;
+            currentContent = mainWindow.contentPanel.Content as UserControl;
+        }
+
+        public static void CloseContent()
+        {
+            OpenContent(previousContent);
+        }
+    }
+}
