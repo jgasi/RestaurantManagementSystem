e70ebae682672455c5b4a192d60563a77b470a3f
juraj <juraj@DESKTOP-QAJPGCA>
2024-05-08 18:53:08 +0200

    Kreiran hamburger menu s clickable tipkom za profil, malo bolji dizajn, nije gotovo.

--- RestaurantManagementSystem/MainWindow.xaml.cs
+++ RestaurantManagementSystem/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace RestaurantManagementSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -33,14 +30,52 @@ namespace RestaurantManagementSystem
             }
         }
 
-        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
-            WindowState = WindowState.Minimized;
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_contacts.Visibility = Visibility.Collapsed;
+                tt_messages.Visibility = Visibility.Collapsed;
+                tt_maps.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+                tt_signout.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_contacts.Visibility = Visibility.Visible;
+                tt_messages.Visibility = Visibility.Visible;
+                tt_maps.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+                tt_signout.Visibility = Visibility.Visible;
+            }
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
