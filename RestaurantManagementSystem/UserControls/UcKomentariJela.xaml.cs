34b7d8a7ad6025b721aa58bd2dc4f83f75dd8a95
jgasi20 <jgasi20@foi.hr>
2024-07-29 21:48:14 +0200

    Dodan scroll view na User control za kontakt informacije kako bi bio pregledan na svim veličinama monitora. Poboljšan dizajn User controla za statistiku i lijepo raspoređen sadržaj. Poboljšan i bolje raspoređen sadržaj na User control za detalje jela i detalje pića.

--- RestaurantManagementSystem/UserControls/UcKomentariJela.xaml.cs
+++ RestaurantManagementSystem/UserControls/UcKomentariJela.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using BusinessLogicLayer.Services;
 using EntitiesLayer.Entities;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace RestaurantManagementSystem.UserControls
 {
@@ -16,7 +13,6 @@ namespace RestaurantManagementSystem.UserControls
         private readonly RecenzijaServices recenzijaServices = new RecenzijaServices();
         private readonly KorisnikServices korisnikServices = new KorisnikServices();
         private ObservableCollection<Recenzija> comments;
-        private static IMemoryCache cache = new MemoryCache(new MemoryCacheOptions());
 
         public UcKomentariJela(Jelo selectedJelo)
         {
@@ -32,24 +28,17 @@ namespace RestaurantManagementSystem.UserControls
             {
                 loadingText.Visibility = Visibility.Visible;
 
-                string cacheKey = $"cache_jelo_{selectedJelo.id_jelo}";
-                if (!cache.TryGetValue(cacheKey, out ObservableCollection<Recenzija> cachedComments))
+                var recenzije = await recenzijaServices.GetRecenzijeByIdAsync(selectedJelo.id_jelo);
+
+                // Očistite postojeće komentare prije dodavanja novih
+                comments.Clear();
+
+                foreach (var recenzija in recenzije)
                 {
-                    var recenzije = await recenzijaServices.GetRecenzijeByIdAsync(selectedJelo.id_jelo);
-                    cachedComments = new ObservableCollection<Recenzija>();
-
-                    foreach (var recenzija in recenzije)
-                    {
-                        var korisnik = korisnikServices.GetKorisnikById(recenzija.Korisnik_id_korisnik);
-                        recenzija.Korisnik = korisnik;
-                        cachedComments.Add(recenzija);
-                    }
-
-                    cache.Set(cacheKey, cachedComments, TimeSpan.FromMinutes(30));
+                    var korisnik = korisnikServices.GetKorisnikById(recenzija.Korisnik_id_korisnik);
+                    recenzija.Korisnik = korisnik;
+                    comments.Add(recenzija);
                 }
-
-                comments = cachedComments;
-                commentsListView.ItemsSource = comments;
 
                 loadingText.Visibility = Visibility.Collapsed;
             }
--- RestaurantManagementSystem/UserControls/UcKomentariPica.xaml.cs
+++ RestaurantManagementSystem/UserControls/UcKomentariPica.xaml.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using BusinessLogicLayer.Services;
 using EntitiesLayer.Entities;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace RestaurantManagementSystem.UserControls
 {
@@ -16,7 +13,6 @@ namespace RestaurantManagementSystem.UserControls
         private readonly RecenzijaServices recenzijaServices = new RecenzijaServices();
         private readonly KorisnikServices korisnikServices = new KorisnikServices();
         private ObservableCollection<Recenzija> comments;
-        private static IMemoryCache cache = new MemoryCache(new MemoryCacheOptions());
 
         public UcKomentariPica(Pice selectedPice)
         {
@@ -32,23 +28,16 @@ namespace RestaurantManagementSystem.UserControls
             {
                 loadingText.Visibility = Visibility.Visible;
 
-                string cacheKey = $"cache_pice_{selectedPice.id_pice}";
-                if (!cache.TryGetValue(cacheKey, out ObservableCollection<Recenzija> cachedComments))
+                var recenzije = await recenzijaServices.GetRecenzijePicaByIdAsync(selectedPice.id_pice);
+                comments.Clear(); // Očisti postojeće komentare
+
+                foreach (var recenzija in recenzije)
                 {
-                    var recenzije = await recenzijaServices.GetRecenzijePicaByIdAsync(selectedPice.id_pice);
-                    cachedComments = new ObservableCollection<Recenzija>();
-
-                    foreach (var recenzija in recenzije)
-                    {
-                        var korisnik = korisnikServices.GetKorisnikById(recenzija.Korisnik_id_korisnik);
-                        recenzija.Korisnik = korisnik;
-                        cachedComments.Add(recenzija);
-                    }
-
-                    cache.Set(cacheKey, cachedComments, TimeSpan.FromMinutes(30));
+                    var korisnik = korisnikServices.GetKorisnikById(recenzija.Korisnik_id_korisnik);
+                    recenzija.Korisnik = korisnik;
+                    comments.Add(recenzija);
                 }
 
-                comments = cachedComments;
                 commentsListView.ItemsSource = comments;
 
                 loadingText.Visibility = Visibility.Collapsed;
