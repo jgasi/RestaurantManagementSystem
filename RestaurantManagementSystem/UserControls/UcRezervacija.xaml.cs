25bc82de2e481fd4a1bf2a4da2de84abbc65a24d
juraj <juraj@DESKTOP-QAJPGCA>
2024-08-15 22:38:25 +0200

    Small fix.

--- RestaurantManagementSystem/UserControls/UcRezervacija.xaml.cs
+++ RestaurantManagementSystem/UserControls/UcRezervacija.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 using BusinessLogicLayer.Services;
 using EntitiesLayer.Entities;
